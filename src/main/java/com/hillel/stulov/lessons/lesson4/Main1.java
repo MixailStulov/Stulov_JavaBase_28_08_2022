ff50ebc40096dc7472e4aeb078af2160f90354c1
Stulov Mykhaylo <hang.wallpapers@gmail.com>
2022-09-11 14:49:52 +0300

    lesson4

--- src/main/java/com/hillel/stulov/lessons/lesson4/Main1.java
+++ src/main/java/com/hillel/stulov/lessons/lesson4/Main1.java
@@ -3,40 +3,25 @@ package com.hillel.stulov.lessons.lesson4;
 public class Main1 {
     public static void main(String[] args) {
 
-        char Li_dynasty_warriors_damage = 'a';
-        char LI_dynasty_archers_damage = 'b';
-        char Li_dynasty_cavalry_damage = 'c';
-        char Li_warriors_total = 'k';
-        char Li_archers_total = 'l';
-        char Li_cavalry_total = 'm';
+        byte li_warriors_damage = 13;
+        byte lI_archers_damage = 24;
+        byte li_cavalry_damage = 46;
+        byte ming_warriors_damage = 9;
+        byte ming_archers_damage = 35;
+        byte ming_cavalry_damage = 12;
 
-        char Ming_dynasty_warriors_damage = 'x';
-        char Ming_dynasty_archers_damage = 'y';
-        char Ming_dynasty_cavalry_damage = 'z';
-        char Ming_warriors_total = 'o';
-        char Ming_archers_total = 'p';
-        char Ming_cavalry_total = 'q';
+        int li_warriors = 860;
+        int li_archers = (li_archers = li_warriors);
+        int li_cavalry = (li_cavalry = li_warriors);
+        double ming_warriors = li_warriors * 1.5;
+        double ming_archers = (ming_archers = ming_warriors);
+        double ming_cavalry = (ming_cavalry = ming_warriors);
 
-        byte a = 13;
-        byte b = 24;
-        byte c = 46;
-        byte x = 9;
-        byte y = 35;
-        byte z = 12;
+        double li_total_damage = (li_warriors_damage + lI_archers_damage + li_cavalry_damage) * (li_warriors);
+        double ming_total_damage = (ming_warriors_damage + ming_archers_damage + ming_cavalry_damage) * (ming_warriors);
 
-        int k = 860;
-        int l = (l = k);
-        int m = (m = k);
-        double o = k * 1.5;
-        double p = (p = o);
-        double q = (q = o);
-
-        int Li_total_damage = (a + b + c) * 860;
-        double Ming_total_damage = (x + y + z) * (o);
-
-        System.out.println("Li total damage = " + Li_total_damage);
-        System.out.println("Ming total damage = " + Ming_total_damage);
-
+        System.out.println("Li total damage = " + li_total_damage);
+        System.out.println("Ming total damage = " + ming_total_damage);
 
 
     }
