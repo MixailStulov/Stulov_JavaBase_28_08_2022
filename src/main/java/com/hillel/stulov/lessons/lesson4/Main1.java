194b8b8461a62a828f75e4a7b0d6da469a11c5a7
Stulov Mykhaylo <hang.wallpapers@gmail.com>
2022-09-11 19:04:15 +0300

    lesson4

--- src/main/java/com/hillel/stulov/lessons/lesson4/Main1.java
+++ src/main/java/com/hillel/stulov/lessons/lesson4/Main1.java
@@ -11,11 +11,11 @@ public class Main1 {
         byte ming_cavalry_damage = 12;
 
         int li_warriors = 860;
-        int li_archers = (li_archers = li_warriors);
-        int li_cavalry = (li_cavalry = li_warriors);
+        int li_archers = (li_warriors);
+        int li_cavalry = (li_warriors);
         double ming_warriors = li_warriors * 1.5;
-        double ming_archers = (ming_archers = ming_warriors);
-        double ming_cavalry = (ming_cavalry = ming_warriors);
+        double ming_archers = (ming_warriors);
+        double ming_cavalry = (ming_warriors);
 
         double li_total_damage = (li_warriors_damage + lI_archers_damage + li_cavalry_damage) * (li_warriors);
         double ming_total_damage = (ming_warriors_damage + ming_archers_damage + ming_cavalry_damage) * (ming_warriors);
