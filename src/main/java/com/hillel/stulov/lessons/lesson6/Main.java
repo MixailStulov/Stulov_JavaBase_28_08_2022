2d7c9bb1235667c839aeaa895bf8cf17feaff25a
Stulov Mykhaylo <hang.wallpapers@gmail.com>
2022-09-20 01:31:28 +0300

    lesson4

--- src/main/java/com/hillel/stulov/lessons/lesson6/Main.java
+++ src/main/java/com/hillel/stulov/lessons/lesson6/Main.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner game = new Scanner(System.in);
+
         System.out.print("Guess the number from 1 to 10?:");
         int count;
         int a = 1;
         int b = 10;
         int randomNumber = a + (int) (Math.random() * b);
+
         System.out.println();
+
         for (int i = 1; i < 4; i++) {
             count = game.nextInt();
             if (count < randomNumber) {
