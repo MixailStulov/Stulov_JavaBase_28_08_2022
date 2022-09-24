bd123a2f87bc64c93094e9315004e843d4c2f691
Stulov Mykhaylo <hang.wallpapers@gmail.com>
2022-09-25 02:32:20 +0300

    lesson4

--- src/main/java/com/hillel/stulov/lessons/lesson6/Main1.java
+++ src/main/java/com/hillel/stulov/lessons/lesson6/Main1.java
@@ -19,22 +19,3 @@ public class Main1 {
         }
     }
 }
-//    int counter = 0;
-//        for (int i = 1; counter < 100; i++) {
-//            if (String.valueOf(i).contains("4") || String.valueOf(i).contains("9")) {
-//                continue;
-//            } else {
-//                System.out.println("number: " + i);
-//                counter++;
-//            }
-//        }
-//        System.out.println("counter = " + counter);
-
-    /*int n = 0, i = 0;
-        while (n < 100) {
-        if (!String.valueOf( i ).contains( "4" ) || !String.valueOf( i ).contains( "9" )) {
-        System.out.println(i);
-        n++;
-        }
-        i++;
-        } */
