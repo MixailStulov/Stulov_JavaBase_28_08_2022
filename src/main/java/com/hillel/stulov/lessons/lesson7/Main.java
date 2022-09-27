afd14d2863d64e396f69b83942c419d563b6866f
Stulov Mykhaylo <hang.wallpapers@gmail.com>
2022-09-27 22:55:10 +0300

    lesson4

--- src/main/java/com/hillel/stulov/lessons/lesson7/Main.java
+++ src/main/java/com/hillel/stulov/lessons/lesson7/Main.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] team1 = new int[25];
-        int resultTeam1 = 0;
+        final int LENGHT = 25;
+        int[] team1 = new int[LENGHT];
+        int sum1 = 0;
         for (int i = 0; i < team1.length; i++) {
             team1[i] = (int) (Math.random() * 23) + 18;
-            resultTeam1 = resultTeam1 + team1[i];
-            resultTeam1 = resultTeam1 / 25;
+            sum1 += team1[i];
             if (i == team1.length - 1) {
                 System.out.print(team1[i]);
             } else {
@@ -19,11 +19,10 @@ public class Main {
         }
         System.out.println();
         int[] team2 = new int[25];
-        int resultTeam2 = 0;
+        int sum2 = 0;
         for (int i = 0; i < team2.length; i++) {
             team2[i] = (int) (Math.random() * 23) + 18;
-            resultTeam2 = resultTeam2 + team2[i];
-            resultTeam2 = resultTeam2 / 25;
+            sum2 += team2[i];
             if (i == team2.length - 1) {
                 System.out.print(team2[i]);
             } else {
@@ -31,8 +30,9 @@ public class Main {
             }
         }
         System.out.println();
-        System.out.println( "average score team1 :" + resultTeam1);
-        System.out.println("average score team2 :" + resultTeam2);;
+        System.out.println( "average score team1 :" + sum1/LENGHT);
+        System.out.println("average score team2 :" + sum2/LENGHT);
+        System.out.println(" ");
     }
 }
 
