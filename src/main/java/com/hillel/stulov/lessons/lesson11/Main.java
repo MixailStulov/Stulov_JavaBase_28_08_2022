2124210b3259276ccaf28402d440e5f6e2d3cd01
Stulov Mykhaylo <hang.wallpapers@gmail.com>
2022-10-05 15:07:36 +0300

    lesson4

--- src/main/java/com/hillel/stulov/lessons/lesson11/Main.java
+++ src/main/java/com/hillel/stulov/lessons/lesson11/Main.java
@@ -2,12 +2,10 @@ package com.hillel.stulov.lessons.lesson11;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println(personInfo("Homer", "Simpson", "Springfield.", "(800) KL5-3226"));
         System.out.println(personInfo("Ghost", "Busters", "Ney York.", "(800) 555-2368"));
         System.out.println(personInfo("Fox", "Mulder", " Martha's Vineyard, Massachusetts.", "(800) 555-0199"));
     }
-
     static String personInfo(String firstName, String lastName, String city, String number) {
         return "Call to " + firstName + " " + lastName + " from the " + city + " By number:" + number + ".";
     }
